ff705e1ca5f8aad648df23340c97dd58bf002031
Tim Evers <tevers@sogyo.nl>
2017-06-13 13:46:07 +0200

    Removed HomeController, functionality now in PatternController.

--- app/controllers/PatternController.scala
+++ app/controllers/PatternController.scala
@@ -8,13 +8,13 @@ import play.api.data.Forms._
 import play.api.Play.current
 import play.api.i18n.Messages.Implicits._
 import nl.sogyo.kbd.db._
-import nl.sogyo.kbd.domain.Pattern
+import nl.sogyo.kbd.domain.{Pattern, Track}
 import nl.sogyo.kbd.forms._
 
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
-abstract class PatternController @Inject()(pc: PatternCollection, sc: SoundCollection) extends Controller {
+class PatternController @Inject()(pc: PatternCollection, sc: SoundCollection) extends Controller {
 
   val patternForm = Form(
     mapping(
@@ -27,6 +27,10 @@ abstract class PatternController @Inject()(pc: PatternCollection, sc: SoundColle
     )(PatternForm.apply)(PatternForm.unapply)
   )
 
+  def index: Action[AnyContent] = {
+    fromPatternID(1)
+  }
+
   def fromPatternID(patternID: Int): Action[AnyContent] = Action.async { implicit request =>
     pc.get(patternID).flatMap {
       case Some(p) => createResult(p)
@@ -37,6 +41,25 @@ abstract class PatternController @Inject()(pc: PatternCollection, sc: SoundColle
   def createResult(p: Pattern)(implicit rc: RequestHeader): Future[Result] = {
     val filledForm = patternForm.fill(PatternForm(p.name, p.data.map(_.data), p.data.map(_.name), p.data.map(_.sound.name), p.length, p.tracks))
     val soundMap = p.generateSoundMap
-    sc.getAllNames.map(_.sorted).map(_.map(Some(_))).map(names => Ok(views.html.index(soundMap, names)(filledForm)))
+    sc.getAllNames.map(_.sorted).map(_.map(Some(_))).map(names => Ok(views.html.index(soundMap, names, filledForm)))
+  }
+
+  def postPattern: Action[AnyContent] = Action.async { implicit request =>
+    patternForm.bindFromRequest.fold(
+      formWithErrors => {
+        Future.successful(BadRequest(formWithErrors.toString))
+      },
+      pForm => {
+        val pattern: Future[Pattern] = {
+          val tracks: Future[Seq[Track]] = Future.sequence(for {
+            ((data, name), soundName) <- pForm.data.zip(pForm.trackNames).zip(pForm.trackSounds)
+          } yield sc.get(soundName).map(soundOption => Track(name, soundOption.get, (data ++ Seq.fill(pForm.length - data.length)(false)).take(pForm.length): _*)))
+          val paddedTracks = tracks.map(ts => (ts ++ Seq.fill(pForm.tracks - ts.length)(Track.empty(pForm.length))).take(pForm.tracks))
+          paddedTracks.map(ts => Pattern(pForm.name, ts: _*))
+        }
+        val fid = pattern.flatMap(pc.post)
+        fid.map(id => Redirect(routes.PatternController.fromPatternID(id)))
+      }
+    )
   }
 }
